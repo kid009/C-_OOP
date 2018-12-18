86f6f925a424c0f31abedd9fd020e863518a25de
kid009 <pongpoom1985@gmail.com>
2018-12-18 18:38:30 +0700

    Add OOP_BOOK_EXAMPLE

---
+++ OOP_BOOK/OOP_BOOK/ch1/Foo01a.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OOP_BOOK.ch1
+{
+    public class Foo01a
+    {
+        //field
+        private string test;
+        //property
+        
+    }
+}
